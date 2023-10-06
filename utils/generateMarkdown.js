fba319b0e3f85d585d47eb5bbe3bf8fbfd4ce853
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-10-06 18:57:33 +0100

    Added a function to generate a badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
+//
+// CREATE A FUNCTION TO GENERATE A LICENSE BADGE
+//
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github license] (https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)};
 
 `;
 }
