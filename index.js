1d18f6dfaf47494adb017d57f5038482d57133e3
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-10-06 19:21:58 +0100

    Added the questions to the index.js

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your projects name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project...",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license do you want?",
+    choices: ["MIT", "APACHE 2.0", "GPL", "NONE"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run test?",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
